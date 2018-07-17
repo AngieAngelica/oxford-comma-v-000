2314d38e0e843be223290062efb5ed2b75337a8c
Angie Kassab <anasab12@gmail.com>
2018-07-17 18:06:23 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,15 @@
 something = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
-  if array.length == 2
-  array[-2] << " and "
-  array.join
-  elsif array.length == 1
-  	array.join
-  elsif array.length > 2
-    array[-1].prepend "and "
-	array.join(", ")
-  end
+#   if array.length == 2
+#   array[-2] << " and "
+#   array.join
+#   elsif array.length == 1
+#   	array.join
+#   elsif array.length > 2
+#     array[-1].prepend "and "
+# 	array.join(", ")
+#   end
 end
 
 oxford_comma(something)
