9f0652c2378cce1cb3ef0aaa6f8e4de8331558a3
Angie Kassab <anasab12@gmail.com>
2018-07-17 18:21:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,12 @@
+require "pry"
+
 something = ["fiddleheads","okra","kohlrabi"]
 
 def oxford_comma(array)
+  binding.pry 
   if array.length == 1
     array.join
+  end
 #   if array.length == 2
 #   array[-2] << " and "
 #   array.join
